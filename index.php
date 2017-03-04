3e92f1a698b60fd022a217e2097b855a430d973c
John Pepp <jrpepp@pepster.com>
2017-03-04 09:59:24 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -12,21 +12,21 @@
     </head>
     <body>
         <div class="container">
-            <form id="register" action="index.php" method="post">
+            <form id="register" action="index.php" method="post" autocomplete="off">
                 <fieldset>
                     <legend>Registration Form</legend>
                     <input type="hidden" name="action" value="submit">
-                    <label for="username">username</label>
-                    <input id="username" type="text" name="username" value="">
-                    <label for="password">password</label>
-                    <input id="password" type="password" name="password">
-                    <label for="verify">verify password</label>
-                    <input id="verify" type="password" name="verify">
-                    <label for="email">email address</label>
-                    <input id="email" type="text" name="email" value="">
-                    <label for="verify_email">verify email</label>
-                    <input id="verify_email" type="text" name="verifyEmail" value="">
-                    <input id="registerBtn" type="submit" name="submit" value="submit">
+                    <label for="username">username:</label>
+                    <input id="username" type="text" name="username" value="" autofocus tabindex="1">
+                    <label for="password">password:</label>
+                    <input id="password" type="password" name="password" tabindex="2">
+                    <label for="verify">verify password:</label>
+                    <input id="verify" type="password" name="verify" tabindex="3">
+                    <label for="email">email address:</label>
+                    <input id="email" type="text" name="email" value="" tabindex="4">
+                    <label for="verify_email">verify email:</label>
+                    <input id="verify_email" type="text" name="verifyEmail" value="" tabindex="5">
+                    <input id="registerBtn" type="submit" name="submit" value="submit" tabindex="6">
                 </fieldset>
             </form>
         </div>
